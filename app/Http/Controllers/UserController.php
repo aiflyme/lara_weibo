c2b4758d4023cc5d9cdebca0323e0527bcbd9f59
Robin Chen <aiflyme@gmail.com>
2024-04-09 17:00:02 +0200

    6.2 user display page

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    public function index()
+    {
+        return view('users.create');
+    }
+
     public function create()
     {
         return view('users.create');
     }
+
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+
 }
+
--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,4 @@ Route::get('/', [StaticPagesController::class, 'home'])->name('home');
 Route::get('/help', [StaticPagesController::class, 'help'])->name('help');
 Route::get('/about', [StaticPagesController::class, 'about'])->name('about');
 
-Route::get('signup', [UserController::class, 'create'])->name('signup');
+Route::resource('user', UserController::class);
