c4664efe6685e379ab9d0e9e682fc73944347fa9
Robin Chen <aiflyme@gmail.com>
2024-04-11 18:25:21 +0200

    uesr paginate

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -20,7 +20,7 @@ class UserController extends Controller
 
     public function index()
     {
-        $users = User::all();
+        $users = User::paginate(3);
         return view('users.index', compact('users')) ;
     }
 
---
+++ resources/views/users/_user.blade.php
@@ -0,0 +1,8 @@
+<div class="list-group-item">
+    <img class="me-3" src="{{ $user->gravatar() }}" alt="{{ $user->name }}" width=32>
+    <a href="{{ route('user.show', $user) }}">
+        {{ $user->name }}
+    </a>
+
+
+</div>
