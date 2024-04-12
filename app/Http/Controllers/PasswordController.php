2588bb59787b1ecae642d00b60a47f04b0d0c068
Robin Chen <aiflyme@gmail.com>
2024-04-12 13:28:30 +0200

    reset password

--- app/Http/Controllers/PasswordController.php
+++ app/Http/Controllers/PasswordController.php
@@ -54,8 +54,62 @@ class PasswordController extends Controller
         return redirect()->back();
     }
 
-    public function reset()
+
+    public function showResetForm(Request $request)
     {
-        
+        $token = $request->route()->parameter('token');
+        return view('auth.passwords.reset', compact('token'));
+    }
+
+    public function reset(Request $request)
+    {
+git
+        $request->validate([
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|confirmed|min:8',
+        ]);
+
+        $email = $request->email;
+        $token = $request->token;
+
+        // 找回密码链接的有效时间
+        $expires = 60 * 10;
+
+        // get user information
+        $user = User::where('email', $email)->first();
+
+        //check is exist user
+        if(is_null($user)){
+            session()->flash('danger', 'email is not register or wrong');
+            return redirect()->back()->withInput();
+        }
+
+        //get the reset record from DB
+        $record = (array)DB::table('password_reset_tokens')->where('email' ,$email)->first();
+
+        //find record
+        if($record){
+            //check date
+            if(Carbon::parse($record['created_at'])->addSecond($expires)->isPast()){
+                session()->flash('danger', '链接已过期，请重新尝试');
+                return redirect()->back();
+            }
+            //check the token is right
+            if(! Hash::check($token, $record['token'])){
+                session()->flash('danger', '令牌错误');
+                return redirect()->back();
+            }
+
+            //update user password
+            $user->update(['password' => bcrypt($request->password)]);
+            session()->flash('success', 'update you password success');
+            return redirect()->route('login');
+
+        }
+
+        // 6. 记录不存在
+        session()->flash('danger', '未找到重置记录');
+        return redirect()->back();
     }
 }
